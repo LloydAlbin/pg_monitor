d77ec3478917c5971160a0c1ba2f17b544a912b3
Lloyd Albin <lalbin@fredhutch.org>
2019-10-21 11:13:48 -0700

    Fixed typo
    Updated readlog.sh to pg_readlog.sh

--- readlog/start_pg_readlog.sh
+++ readlog/start_pg_readlog.sh
@@ -1,1 +1,1 @@
-nohup readlog.sh db-delphi kw-alpha-m03.pc.scharp.org reports 30002 /pgdata_local/pg_log /pgdata_local /usr/local/postgres-current/bin &
+nohup pg_readlog.sh db-delphi kw-alpha-m03.pc.scharp.org reports 30002 /pgdata_local/pg_log /pgdata_local /usr/local/postgres-current/bin &
