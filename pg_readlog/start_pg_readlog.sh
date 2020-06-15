5e7512d070fc618bfcb8c171075080e8684a0437
Lloyd Albin <lalbin@fredhutch.org>
2020-06-15 15:05:19 -0700

    Update start_pg_readlog.sh
    Grab the hostname automatically instead of hard coding it.

--- pg_readlog/start_pg_readlog.sh
+++ pg_readlog/start_pg_readlog.sh
@@ -1,1 +1,2 @@
-nohup pg_readlog.sh <cluster_name> <timescaledb_server> pgmonitor_db 30002 /pgdata_local/pg_log /pgdata_local /usr/local/postgres-current/bin &
+hostname=$(hostname -s)
+nohup pg_readlog.sh $hostname <timescaledb_server> pgmonitor_db 30002 /pgdata_local/pg_log /pgdata_local /usr/local/postgres-current/bin &
