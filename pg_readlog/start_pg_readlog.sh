a6afc4d6a58f3b8ed9fda2a425cdce04766b4f8d
Lloyd Albin <lloyd@thealbins.com>
2024-06-05 14:56:49 -0700

    Update start_pg_readlog.sh
    Adding 2 new variables

--- pg_readlog/start_pg_readlog.sh
+++ pg_readlog/start_pg_readlog.sh
@@ -1,2 +1,2 @@
 hostname=$(hostname -s)
-nohup pg_readlog.sh $hostname <timescaledb_server> pgmonitor_db 30002 /pgdata_local/pg_log /pgdata_local /usr/local/postgres-current/bin &
+nohup pg_readlog.sh $hostname <timescaledb_server> pgmonitor_db 30002 /pgdata_local/pg_log /pgdata_local /usr/local/postgres-current/bin logtail.csv 5 &
